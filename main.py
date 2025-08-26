b7fdd0d23fd095f57d464a984e7291eb3ed41148
Evan Liu <4542elgh@gmail.com>
2025-08-26 15:12:12 -0700

    reduce verbosity

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import os
+import logging
 import shutil
 import subprocess
 from datetime import datetime
 from OpenSSL import crypto
 import requests
 import config
-import logging
 
 logger = logging.getLogger(__name__)
-print(config.VERBOSE)
-if config.VERBOSE:
-    if config.VERBOSE.lower() == "debug":
-        logging.basicConfig(level=logging.DEBUG)
-    elif config.VERBOSE.lower() == "info":
-        logging.basicConfig(level=logging.INFO)
+
+def setup_logging():
+    format_str = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    if config.VERBOSE:
+        if config.VERBOSE.lower() == "debug":
+            logging.basicConfig(level=logging.DEBUG, format=format_str)
+        elif config.VERBOSE.lower() == "info":
+            logging.basicConfig(level=logging.INFO, format=format_str)
+        else:
+            # This is for "Error" value or anything else
+            logging.basicConfig(level=logging.ERROR, format=format_str)
     else:
-        # This is for "Error" value or anything else
-        logging.basicConfig(level=logging.ERROR)
-else:
-    logging.basicConfig(level=logging.ERROR)
+        logging.basicConfig(level=logging.ERROR, format=format_str)
 
 def dc_alert(payload:str) -> None:
     json = {}
     headers = {}
 
     if config.NOTIFICATION_SERVICE and config.WEBHOOK_URL:
+        logger.debug("Notification service: %s and webhook url: %s", config.NOTIFICATION_SERVICE, config.WEBHOOK_URL)
         if config.NOTIFICATION_SERVICE.lower() == "apprise":
             json = {
                 "body": payload,
@@ -38,15 +41,19 @@ def dc_alert(payload:str) -> None:
                 "Content-Type": "application/json"
             }
 
-        logger.debug("Requests sending to %s\n\tpayload: %s\n\theaders: %s\n\t10 second timeout", config.WEBHOOK_URL, json, headers)
-        logger.info("Discord message sent!")
-        requests.post(config.WEBHOOK_URL, json, headers, timeout=10)
+        try:
+            response = requests.post(config.WEBHOOK_URL, json=json, headers=headers, timeout=10)
+            response.raise_for_status()
+            logger.info("Notification sent successfully")
+        except requests.RequestException as e:
+            logger.error("Failed to send notification: %s", e)
+    else:
+        logger.info("Skip Discord notification!")
 
-def main():
+def get_remote_cert():
     # Check if remote is ssh accessible
     try:
         cmd = ['ssh',"-T",f"root@{config.REMOTE_URL}", "exit"]
-        logger.debug("Checking remote ssh is accessible: %s", cmd)
         subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
     except subprocess.CalledProcessError as e:
         # In case this message does not get rendered, f-string will still cause a render anyway, while %s rendering will be determine by logger class
@@ -54,86 +61,83 @@ def main():
         dc_alert(f"Error while running script: {e}")
         exit(1)
 
-    # Get latest cert from VPS to Docker NGINX Cert folder
+    # Check remote have cert at all
     cmd = ["ssh", f"root@{config.REMOTE_URL}", f"cd {config.REMOTE_CERT_LOCATION} && ls"]
-    logger.debug("Getting certs: %s", cmd)
     process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+    logger.debug("Got %s certificates from remote", int(len(process.stdout.strip().split("\n"))/2)) # Fullchain and PrivKey is a pair
     if (len(process.stdout.strip().split("\n")) == 0):
-        logger.error("Cert location does not have any file, exiting")
-        dc_alert("Cert location does not have any file, exiting")
+        logger.error("Cert location does not have any certificate, exiting")
+        dc_alert("Cert location does not have any certificate, exiting")
         exit(0)
 
     # Remove local certs
+    local_cert_dir = os.listdir(config.LOCAL_CERT_LOCATION)
     logger.info("Removing %s files", config.LOCAL_CERT_LOCATION)
-    logger.debug("Files in %s", config.LOCAL_CERT_LOCATION)
-    for f in os.listdir(config.LOCAL_CERT_LOCATION):
-        logger.debug(f)
+    logger.debug("Removing %s", local_cert_dir)
+    for f in local_cert_dir:
         os.remove(os.path.join(config.LOCAL_CERT_LOCATION, f))
 
-    logger.info("Rsync certbot certificates from %s", config.REMOTE_CERT_LOCATION)
+    # Rsync certs to local
+    logger.info("Rsync certificates from %s", config.REMOTE_CERT_LOCATION)
     try:
         cmd = ["rsync", "--archive", f"root@{config.REMOTE_URL}:{config.REMOTE_CERT_LOCATION}/privkey*.pem", config.LOCAL_CERT_LOCATION]
-        logger.debug("rsync privkey: %s", cmd)
         subprocess.run(cmd, check=True)
 
         cmd = ["rsync", "--archive", f"root@{config.REMOTE_URL}:{config.REMOTE_CERT_LOCATION}/fullchain*.pem", config.LOCAL_CERT_LOCATION ]
-        logger.debug("rsync fullchain: %s", cmd)
         subprocess.run(cmd, check=True)
     except subprocess.CalledProcessError as e:
-        dc_alert(f"Error while running rsync: {e}")
         # In case this message does not get rendered, f-string will still cause a render anyway, while %s rendering will be determine by logger class
         logger.error("Error while running rsync: %s", e)
+        dc_alert(f"Error while running rsync: {e}")
         exit(1)
 
+def update_to_latest_cert():
+    logger.info("Sorting by latest certificate and replace existing cert at %s", config.LOCAL_CERT_LOCATION)
     # Sort by cert's numbering if there are multiple
-    logger.info("Sorting latest certbot certificates if multiple presents")
     all_cert = os.listdir(config.LOCAL_CERT_LOCATION)
-    logger.debug("Certs in %s %s", config.LOCAL_CERT_LOCATION, all_cert)
 
     # If have number, remove the string and sort by number, get largest int (latest cert), and combine back to string
     priv = [cert for cert in all_cert if "privkey" in cert]
-    logger.debug("All private keys: %s", priv)
     priv = "privkey" + str(sorted([int(i.replace("privkey","").replace(".pem", "")) for i in priv])[-1]) + ".pem"
-    logger.debug("Choosing %s", priv)
+    logger.debug("Found latest privkey: %s", priv)
 
     fullchain = [cert for cert in all_cert if "fullchain" in cert]
-    logger.debug("All fullchain keys: %s", fullchain)
     fullchain = "fullchain" + str(sorted([int(i.replace("fullchain","").replace(".pem", "")) for i in fullchain])[-1]) + ".pem"
-    logger.debug("Choosing %s", fullchain)
+    logger.debug("Found latest fullchain: %s", fullchain)
 
-    # Rename the latest privkey and fullchain strip out the number
-    logger.info("Renaming latest privkey and fullchain")
-
-    logger.debug("Copying %s to %s", config.LOCAL_CERT_LOCATION + "/" + priv, config.LOCAL_CERT_LOCATION + "/privkey.pem")
     shutil.copy2(f"{config.LOCAL_CERT_LOCATION}/{priv}", f"{config.LOCAL_CERT_LOCATION}/privkey.pem")
-
-    logger.debug("Copying %s to %s", config.LOCAL_CERT_LOCATION + "/" + fullchain, config.LOCAL_CERT_LOCATION + "/fullchain.pem")
     shutil.copy2(f"{config.LOCAL_CERT_LOCATION}/{fullchain}", f"{config.LOCAL_CERT_LOCATION}/fullchain.pem")
 
     # Remove everything in local except new cert
+    local_cert_dir = [cert for cert in os.listdir(config.LOCAL_CERT_LOCATION) if cert != "privkey.pem" and cert != "fullchain.pem"]
     logger.info("Remove everything but latest privkey and fullchain")
-    logger.debug("Files in %s", config.LOCAL_CERT_LOCATION)
-    for f in os.listdir(config.LOCAL_CERT_LOCATION):
-        if f!="privkey.pem" and f!="fullchain.pem":
-            logger.debug(f)
-            os.remove(os.path.join(config.LOCAL_CERT_LOCATION, f))
+    logger.debug("Removing %s", local_cert_dir)
+    for f in local_cert_dir:
+        os.remove(os.path.join(config.LOCAL_CERT_LOCATION, f))
 
-    # Notify in Discord the new expiration date
-    logger.debug("Checking expire date on %s", config.LOCAL_CERT_LOCATION + "/fullchain.pem")
-    with open(f'{config.LOCAL_CERT_LOCATION}/fullchain.pem', 'rb') as fullchain_pem:
-        logger.debug("Decrypting with crypto.FILETYPE_PEM")
-        cert = crypto.load_certificate(crypto.FILETYPE_PEM, fullchain_pem.read())
-        not_after = cert.get_notAfter()
-        if not_after:
-            logger.debug("Decode with UTF-8")
-            not_after = not_after.decode('utf-8')
-            expire_time = datetime.strptime(not_after,"%Y%m%d%H%M%SZ")
-            logger.debug("Converting date %s with formatter: %%Y%%m%%d%%H%%M%%SZ to %s", not_after, expire_time)
-            logger.info("Certificate will expire on: %s", expire_time)
-            dc_alert(f"Grabbed new Let's Encrypt certificates, expire on {expire_time}")
-        else:
-            logger.error("Failed to get expiration date from the certificate.")
-            dc_alert("Failed to get expiration date from the certificate.")
+def main():
+    logger.info("Starting certificate synchronization from %s to %s", config.REMOTE_CERT_LOCATION, config.LOCAL_CERT_LOCATION)
+    get_remote_cert()
+    update_to_latest_cert()
+
+    try:
+        # Notify in Discord the new expiration date
+        with open(f'{config.LOCAL_CERT_LOCATION}/fullchain.pem', 'rb') as fullchain_pem:
+            cert = crypto.load_certificate(crypto.FILETYPE_PEM, fullchain_pem.read())
+            not_after = cert.get_notAfter()
+            if not_after:
+                not_after = not_after.decode('utf-8')
+                expire_time = datetime.strptime(not_after,"%Y%m%d%H%M%SZ")
+                logger.info("Certificate %s will expire on: %s", config.LOCAL_CERT_LOCATION+"/fullchain.pem", expire_time)
+                dc_alert(f"Grabbed new Let's Encrypt certificates, expire on {expire_time}")
+            else:
+                logger.error("Failed to get expiration date from the certificate %s.", config.LOCAL_CERT_LOCATION+"/fullchain.pem")
+                dc_alert("Failed to get expiration date from the certificate.")
+    except (FileNotFoundError, IOError) as e:
+        logger.error("Certificate failed parsing with exception %s", e)
+        dc_alert(f"Certificate failed parsing with exception {e}")
+        exit(1)
 
 if __name__ == "__main__":
+    setup_logging()
     main()
